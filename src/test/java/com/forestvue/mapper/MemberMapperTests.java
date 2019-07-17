05b99f19ce33655fa02f111a2dcc70472fa86126
Choi Seungeon <chou610@naver.com>
2019-07-17 23:47:36 +0900

    [update] delete member & auth before member insertion test

--- src/test/java/com/forestvue/mapper/MemberMapperTests.java
+++ src/test/java/com/forestvue/mapper/MemberMapperTests.java
@@ -6,12 +6,14 @@ import com.forestvue.domain.AuthVO;
 import com.forestvue.domain.MemberVO;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {RootConfig.class, SecurityConfig.class})
@@ -22,21 +24,25 @@ public class MemberMapperTests {
 
     @Setter(onMethod_ = @Autowired)
     private PasswordEncoder pwencoder;
+    @Before
+    public void delete(){
+        mapper.deleteUserAuthAll();
+        mapper.deleteUserAll();
+    }
 
     @Test
     public void insert(){
         MemberVO member = new MemberVO();
-        member.setUserName("ex00");
-        member.setUserpw(pwencoder.encode("pw00"));
+        member.setUserName("spring");
+        member.setUserpw(pwencoder.encode("mayflower123!"));
         mapper.write(member);
-        log.info(member);
+        AuthVO auth = new AuthVO();
+        auth.setAuth("ROLE_ADMIN");
+        auth.setUserid(member.getUserid());
+        mapper.grant(auth);
     }
     @Test
     public void grant(){
-        AuthVO auth = new AuthVO();
-        auth.setAuth("ROLE_ADMIN");
-        auth.setUserid("1");
-        mapper.grant(auth);
-        log.info(auth);
+
     }
 }
